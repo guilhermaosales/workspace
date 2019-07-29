dee3241279b93a07ff8ee4c209b0c6bbaf8cdca1
Guilherme Sales <guilhermewrsales@gmail.com>
2019-07-29 13:20:43 -0300

    new updates in app.js

--- aprendendo_javascript/app.js
+++ aprendendo_javascript/app.js
@@ -1,2 +1,19 @@
-console.log('hello world'); 
-console('Esse não funciona');
+const usuario = "anônimo";
+const mensagem = "";
+
+const mensagemValida = mensagem.length > 140;
+const usuarioValido = usuario.length > 100 || usuario === "anônimo";
+const todosOsRequisitosValidos = mensagemValida && usuarioValido;
+const mensagemInvalida = !mensagemValida && usuarioValido;
+const usuarioInvalido = mensagemValida && !usuarioValido;
+
+if (todosOsRequisitosValidos) {
+    console.log("Enviar a mensagem");
+} else if (mensagemInvalida) {
+    console.log("A mensagem não pode ser enviada, pois possui menos de 140 caracteres.");
+} else if (usuarioInvalido){
+    console.log("A mensagem não pode ser enviada, pois o nome de usuário é inválido.");
+} else {
+    console.log("A mensagem não pode ser enviada, pois não atende os requisitos para uso.");
+}
+    
